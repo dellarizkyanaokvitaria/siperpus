efd6cf7bcd103ee0cecfae35f28bc810bed66891
dellarizkyanaokvitaria <63853220+dellarizkyanaokvitaria@users.noreply.github.com>
2020-04-17 21:44:39 +0700

    Add files via upload

---
+++ transaksi/hapus.php
@@ -0,0 +1,23 @@
+<?php 
+include '../koneksi.php';
+$id=$_GET["id_pinjam"];
+$query1=mysqli_query($kon,"DELETE FROM detail_pinjam where id_pinjam='$id'");
+$query2=mysqli_query($kon,"DELETE FROM peminjaman where id_pinjam='$id'");
+
+
+// var_dump($query1&&$query2);
+
+if(($query1&&$query2)==1){
+    echo
+            "
+            <script>
+                alert('Data Berhasil Dihapus!');
+                document.location.href = 'index.php';
+            </script>
+            ";
+}
+// else{
+//     header("location : index.php");
+//     exit;
+// }
+?>
